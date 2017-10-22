036780635449b4d90108e425aa6c55b503fee0c8
LuanaBelussoUtfpr <belusso.lua@gmail.com>
2017-10-22 02:39:11 -0200

    Ajustes Buscar Carona

--- Ride/src/java/modelo/BuscarCaronaMB.java
+++ Ride/src/java/modelo/BuscarCaronaMB.java
@@ -2,20 +2,42 @@
 package modelo;
 
 import entidade.Carona;
-import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import negocio.BuscarCaronaBean;
 import negocio.IBuscarCarona;
 
 @ManagedBean
 @ViewScoped
 public class BuscarCaronaMB{
-    private IBuscarCarona BuscarCaronaBean;
     private List<Carona> caronas;
+    private Date dt;
+    private String descricao;
+    
+    @EJB
+    private IBuscarCarona buscarCaronaBean;
     
     public List<Carona> consultar(){
-        return BuscarCaronaBean.consultar();
+        return buscarCaronaBean.consultar();
     }
+    
+    public Date getDt() {
+        return dt;
+    }
+
+    public void setDt(Date dt) {
+        this.dt = dt;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
 }
