1fd04d64eef91d3b7bf190c05797cc129fbfe57d
LuanaBelussoUtfpr <belusso.lua@gmail.com>
2017-10-22 02:01:45 -0200

    messages

--- Ride/src/java/entidade/Usuario.java
+++ Ride/src/java/entidade/Usuario.java
@@ -3,17 +3,13 @@ package entidade;
 import java.io.Serializable;
 import java.util.Objects;
 import javax.persistence.Entity;
-import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.PersistenceContext;
 
 @Entity
 public class Usuario implements Serializable{
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String sobrenome;
