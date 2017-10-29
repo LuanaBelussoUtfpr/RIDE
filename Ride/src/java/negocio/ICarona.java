021071592432edd90335e373547cf920279a10af
LuanaBelussoUtfpr <belusso.lua@gmail.com>
2017-10-29 20:17:34 -0200

    Local origem/destino

--- Ride/src/java/negocio/ICarona.java
+++ Ride/src/java/negocio/ICarona.java
@@ -4,8 +4,9 @@ import javax.ejb.Local;
 
 @Local
 public interface ICarona {
-    boolean criar (String descricao, String localOrigem, String localDestino, 
-            Long vagasDisponiveis, Long valorCarona,String dataCarona, String horarioCarona,
-            String cidade, String estado);
+    public boolean criar(String descricao, String localOrigem, String localDestino, 
+            Long vagasDisponiveis, Long valorCarona,String dataCarona, String horarioCarona, 
+            String cidadeOrigem, String estadoOrigem, String paisOrigem,
+            String cidadeDestino, String estadoDestino, String paisDestino);
     
 }
