846befa4484b231f7121668d99bd0baa6be5c706
LuanaBelussoUtfpr <belusso.lua@gmail.com>
2017-11-04 14:17:26 -0200

    Tipos de dados

--- Ride/src/java/negocio/ICarona.java
+++ Ride/src/java/negocio/ICarona.java
@@ -1,11 +1,13 @@
 package negocio;
 
+import java.sql.Time;
+import java.util.Date;
 import javax.ejb.Local;
 
 @Local
 public interface ICarona {
     public boolean criar(String descricao, String localOrigem, String localDestino, 
-            Long vagasDisponiveis, Long valorCarona,String dataCarona, String horarioCarona, 
+            Long vagasDisponiveis, double valorCarona, String dataCarona, String horarioCarona, 
             String cidadeOrigem, String estadoOrigem, String paisOrigem,
             String cidadeDestino, String estadoDestino, String paisDestino);
     
