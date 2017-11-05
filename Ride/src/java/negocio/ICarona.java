d30a9b725a01aa8c0d5cb2da8242484ce37efa36
LuanaBelussoUtfpr <belusso.lua@gmail.com>
2017-11-05 14:45:34 -0200

    Gravação telefone e email

--- Ride/src/java/negocio/ICarona.java
+++ Ride/src/java/negocio/ICarona.java
@@ -7,6 +7,6 @@ public interface ICarona {
     public boolean criar(String descricao, String localOrigem, String localDestino, 
             Long vagasDisponiveis, double valorCarona, String dataCarona, String horarioCarona, 
             String cidadeOrigem, String estadoOrigem, String paisOrigem,
-            String cidadeDestino, String estadoDestino, String paisDestino);
-    
+            String cidadeDestino, String estadoDestino, String paisDestino,
+            String telefone, String email);
 }
